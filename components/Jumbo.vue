443d2242380b05311b256bb7f879ebf1a85efa1b
Raysa Aurrecochea <aurrecochearaysa@gmail.com>
2018-08-19 15:37:27 -0500

    add link to home page

--- components/Jumbo.vue
+++ components/Jumbo.vue
@@ -3,7 +3,7 @@
     <h1>{{ title }}</h1>
   <b-jumbotron header="Bootstrap Vue" lead="Bootstrap 4 Components for Vue.js 2" >
   <p>For more information visit website</p>
-  <b-btn variant="primary" href="#">Go to home page</b-btn>
+  <b-btn variant="primary" href="/">Go to home page</b-btn>
   </b-jumbotron>
   </div>
 </template>
